cf37e1074d5dec5a22f49c93628ef096f9dca7a2
Mohan Suri <mohansuri92@gmail.com>
2025-08-24 03:20:09 +0000

    remove explicit registration

--- src/container.ts
+++ src/container.ts
@@ -1,14 +1,5 @@
 import {container} from 'tsyringe';
-import {UserService} from '@/services/user-service';
-import {UserRepository} from '@/repositories/user-repository';
-import { TenantService } from './services/tenant-service';
-import { TenantRepository } from './repositories/tenant-repository';
-import { AuthService } from './services/auth-service';
 
-container.register(UserService, {useClass: UserService});
-container.register(UserRepository, {useClass: UserRepository});
-container.register(TenantService, {useClass: TenantService});
-container.register(TenantRepository, {useClass: TenantRepository});   
-container.register(AuthService, {useClass: AuthService}); 
 
+// register injectibles as required in future
 export { container };
